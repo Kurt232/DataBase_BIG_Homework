785d0470ea401e9b4dc99a377f501392a014f486
duwenjie <532923577@qq.com>
2021-11-30 17:31:18 +0800

    “差得远”

--- DataBaseBigHomeWork/database_module.py
+++ DataBaseBigHomeWork/database_module.py
@@ -47,6 +47,7 @@ def close_connect(db, cursor):
     db.close()
 
 
+# 一些基本的函数
 # 创建各种表
 # table_info is List of string
 # table_info[0] : table_name
@@ -64,22 +65,15 @@ def creat_table(table_info, cursor):
 # 读者表
 # attributes: 读者号 证件号 名字 性别 系号 年级
 def creat_table_reader(cursor):
-    creat_table(["reader", "id_r int not null primary key auto_increment", "certificate int", "name varchar(255)",
-                 "sex enum('male','female') not null", "dept int", "grade int"], cursor)
+    creat_table(["reader", "id_reader int not null primary key auto_increment", "certificate int", "name varchar(255)",
+                 "sex enum('male','female') not null", "dept varchar(255)", "grade int"], cursor)
 
 
-# 借书表
+# 借还书表
 # attributes: 读者号 书籍号 借书日期 需要还书日期
-def creat_table_record_borrow(cursor):
-    creat_table(["record_borrow", "id_r_b int not null primary key auto_increment", "id_r int not null foreign key",
+def creat_table_record(cursor):
+    creat_table(["record", "id_record int not null primary key auto_increment", "id_r int not null foreign key",
                  "id_book int not null foreign key", "date_borrow date", "date_return date"], cursor)
-
-
-# 还书表
-# attributes: 还书记录id 读者id 书籍id 借书日期 实际还书日期
-def creat_table_record_return(cursor):
-    creat_table(["record_return", "id_r_r int not null primary key auto_increment", "id_r int not null foreign key",
-                 "id_book int not null foreign key", "date_borrow date", "date_return data"], cursor)
 
 
 # 书籍表
@@ -95,8 +89,39 @@ def creat_table_book(cursor):
 
 # 各种插入语句
 def insert(info) -> str:
-
+    sql = "(" + info[0]
+    for i in info[1:]:
+        sql += " ," + i
+    sql += ")"
     return sql
+
+
+def insert_execute(sql, db, cursor):
+    cursor.execute(sql)
+    db.commit()
+
+
+# 插入读者表
+def insert_reader(info, db, cursor):
+    sql = "insert into reader (certificate, name, sex, dept, grade) values "
+    sql += insert(info)
+    insert_execute(sql, db, cursor)
+
+
+# 插入书籍表
+def insert_book(info, db, cursor):
+    sql = "insert into book (book_no, book_name, publisher, date_publish, author, abstract) values "
+    sql += insert(info)
+    insert_execute(sql, db, cursor)
+
+
+# 插入借还书表
+# 还书时间无穷算出来
+# 应还书时间 直接基于借书时间算
+def insert_record_borrow(info, db, cursor):
+    sql = "insert into record (id_reader, id_book, date_borrow, date_return) values "
+    sql += insert(info)
+    insert_execute(sql, db, cursor)
 
 
 # 各种查询语句
@@ -127,51 +152,45 @@ def select_sum_book_on(book_no_s, cursor) -> tuple:
     sql = select(["count(book_no)", "book, record_borrow"])
     sql += "where not exists ( select * from record_borrow where id_r_b in"
     sql += "(" + select(["id_r_b", "book"])
-    sql += "where book_no = " + book_no_s + "))"
+    sql += "where book_no = '" + book_no_s + "'))"
     return select_execute(sql, cursor)
 
 
 # 查询读者基本信息
-# 仅支持姓名和证件号查询
-def select_reader(id_r, cursor) -> tuple:
+# 调用时需要保证合法 len(info)!=0 以及内容正确
+# 组合查询信息查询id
+def select_reader_id(info, cursor) -> list:
+    sql = select(["id_reader", "reader"])
+    sql += "where " + info[0] + " = '" + info[1] + "'"
+    for i in range(2, len(info), 2):
+        sql += " and " + info[i] + " = '" + info[i+1] + "'"
+    ls = []
+    for i in select_execute(sql, cursor):
+        ls.append(list(i))
+    return ls
+
+
+# 组合查询所以信息
+def select_reader_all(info, cursor) -> list:
     sql = select(["*", "reader"])
-    sql += "where id_r = " + id_r
-    return select_execute(sql, cursor)
-
-
-def select_reader_name(name, cursor) -> list:
-    sql = select(["id_r", "reader"])
-    sql += "where name = "+name
+    sql += "where " + info[0] + " = '" + info[1] + "'"
+    for i in range(2, len(info), 2):
+        sql += " and " + info[i] + " = '" + info[i + 1] + "'"
     ls = []
     for i in select_execute(sql, cursor):
-        ls.append(select_reader(i, cursor))
+        ls.append(list(i))
     return ls
 
 
-# 证件号不一定是唯一的 有可能录错了
-def select_reader_certificate(certificate, cursor) -> list:
-    sql = select(["id_r", "reader"])
-    sql += "where certificate = " + certificate
-    ls = []
-    for i in select_execute(sql, cursor):
-        ls.append(select_reader(i, cursor))
-    return ls
-
-
-# 查看读者借书信息
-def select_record_borrow(id_r, cursor) -> tuple:
-    sql = select(['*', 'record_borrow'])
-    sql += 'where id_r = '+id_r
-    return select_execute(sql, cursor)
-
-
-# 查看读者还书信息
-def select_record_return(id_r, cursor) -> tuple:
-    sql = select(['*', 'record_return'])
-    sql += 'where id_r = ' + id_r
+# 查看读者借还书信息
+def select_record(id_r, cursor) -> tuple:
+    sql = select(['*', 'record'])
+    sql += "where id_r = '" + id_r + "'"
     return select_execute(sql, cursor)
 
 
 # 各种修改语句
+
+# 各种删除语句
 
 
