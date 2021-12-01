94294f5a3fc8bf4013ff408436073293763dac21
duwenjie <532923577@qq.com>
2021-12-01 10:52:41 +0800

    “管理员行为定义 和 用户行为定义”

--- DataBaseBigHomeWork/database_module.py
+++ DataBaseBigHomeWork/database_module.py
@@ -70,10 +70,11 @@ def creat_table_reader(cursor):
 
 
 # 借还书表
-# attributes: 读者号 书籍号 借书日期 需要还书日期
+# attributes: 读者号 书籍号 借书日期 应该还书还书日期 out_date(初始值为-1 正常还书为0 超期>0)
+# date 按照 yy--mm-dd
 def creat_table_record(cursor):
     creat_table(["record", "id_record int not null primary key auto_increment", "id_r int not null foreign key",
-                 "id_book int not null foreign key", "date_borrow date", "date_return date"], cursor)
+                 "id_book int not null foreign key", "date_borrow date", "date_return date", "out_date int"], cursor)
 
 
 # 书籍表
@@ -127,6 +128,7 @@ def insert_record_borrow(info, db, cursor):
 
 # 各种查询语句
 # 我甚至不需要知道cursor.fetchall()返回类型 -好像是一个tuple
+# 如果是字符串 info中应该是 info = ["'name'", "'sex'"]
 def select(info) -> str:
     sql = "select "+info[0]
     for i in info[1:-2]:
@@ -143,7 +145,7 @@ def select_execute(sql, cursor) -> tuple:
 # 查看某本书籍总数
 def select_sum_book(book_no_s, cursor) -> tuple:
     sql = select(["count(book_no)", "book"])
-    sql += "where book_no = '" + book_no_s + "'"
+    sql += "where book_no = " + book_no_s
     return select_execute(sql, cursor)
 
 
@@ -153,7 +155,7 @@ def select_sum_book_on(book_no_s, cursor) -> tuple:
     sql = select(["count(book_no)", "book, record_borrow"])
     sql += "where not exists ( select * from record_borrow where id_r_b in"
     sql += "(" + select(["id_r_b", "book"])
-    sql += "where book_no = '" + book_no_s + "'))"
+    sql += "where book_no = " + book_no_s + "))"
     return select_execute(sql, cursor)
 
 
@@ -161,9 +163,9 @@ def select_sum_book_on(book_no_s, cursor) -> tuple:
 # 调用时需要保证合法 len(info)!=0 以及内容正确
 # and 组合查询
 def select_and(info) -> str:
-    sql = "where " + info[0] + " = '" + info[1] + "'"
+    sql = "where " + info[0] + " = " + info[1]
     for i in range(2, len(info), 2):
-        sql += " and " + info[i] + " = '" + info[i+1] + "'"
+        sql += " and " + info[i] + " = " + info[i+1]
     return sql
 
 
@@ -190,7 +192,7 @@ def select_reader_all(info, cursor) -> list:
 # 查看读者借还书信息
 def select_record(id_r, cursor) -> tuple:
     sql = select(['*', 'record'])
-    sql += "where id_r = '" + id_r + "'"
+    sql += "where id_r = " + id_r + ""
     return select_execute(sql, cursor)
 
 
@@ -213,6 +215,19 @@ def select_book_id(info, cursor) -> list:
     return ls
 
 
+# 查询超期未还学生
+# info = 还书期限
+def select_out_date(info, cursor) -> list:
+    sql = select(["*", "reader"])
+    sql += "where id_reader in ("
+    sql += select(["id_reader", "record"])
+    sql += "where out_date = -1 and to_days(now()) - to_days(date_borrow) >= " + info
+    ls = []
+    for i in select_execute(sql, cursor):
+        ls.append(list(i))
+    return ls
+
+
 # 各种修改语句
 def update_execute(sql, db, cursor):
     cursor.execute(sql)
@@ -221,6 +236,7 @@ def update_execute(sql, db, cursor):
 
 # 修改书籍基本信息
 # 约束交给调用它的函数
+# attribute 是指各个属性 attribute[0] 是主键 attribute = list[str, str ...]
 # 按照book_id来修改 这样最稳妥 更新界面设置成 展示 原始数据 然后 在原来的基础上更新 所以 info 是所有数据
 def update(table, attribute, info, db, cursor):
     sql = "update " + table + " set"
@@ -242,6 +258,41 @@ def update_book(attribute_book, info, db, cursor):
 
 # 更新借还书表
 # 即完成还书手续
+def update_record(attribute_book, info, db, cursor):
+    update(table="record", attribute=attribute_book, info=info, db=db, cursor=cursor)
+
+
 # 各种删除语句
+# 检查删除条件
+# 使用时保证info的正确性 info[attribute, value]
+def delete_check(info, cursor) -> bool:
+    sql = select([info[0], "record"])
+    sql += "where " + info[0] + " ='" + info[1] + "' and out_date = -1 "
+    result = select_execute(sql, cursor)  # info 的正确性保证 result = (info[0],) or result = (,)
+    return len(result) == 0
 
 
+# 删除已有数据
+def delete(table, info, db, cursor):
+    sql = "delete from " + table + "where " + info[0] + " = '" + info[1] + "'"
+    cursor.execute(sql)
+    db.commit()
+
+
+# 删除已有记录 record 为后面做准备
+def delete_record(info, db, cursor):
+    delete(table="record", info=info, db=db, cursor=cursor)
+
+
+# 删除已有书籍
+def delete_book(id_book, db, cursor):
+    if delete_check(["id_book", id_book], cursor):
+        delete_record(info=["id_book", id_book], db=db, cursor=cursor)
+        delete(table="book", info=["id_book", id_book], db=db, cursor=cursor)
+
+
+# 删除已有成员
+def delete_reader(id_reader, db, cursor):
+    if delete_check(["id_reader", id_reader], cursor):
+        delete_record(info=["id_reader", id_reader], db=db, cursor=cursor)
+        delete(table="reader", info=["id_reader", id_reader], db=db, cursor=cursor)
