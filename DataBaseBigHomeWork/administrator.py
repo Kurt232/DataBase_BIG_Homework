e4d3bf5c58ab298a09667d4a80894e7c302a260c
duwenjie <532923577@qq.com>
2021-12-01 11:11:00 +0800

    “管理员行为定义 和 用户行为定义”

---
+++ DataBaseBigHomeWork/administrator.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------------------------
+    File Name :         administrator                             
+    Description :                                       
+    Author :            Karl                             
+    Date :              2021-12-01                          
+---------------------------------------------------------
+    Change Activity :   2021-12-01
+    
+--------------------------------------------------------- 
+"""
+
+# 定义administrator的行为 进行封装
+
+from database_module import *
+
+class administrator :
